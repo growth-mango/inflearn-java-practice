6d17122bd361e7912681e75c2191a193c6db334e
mango <esudatcom@gmail.com>
2024-02-27 17:58:09 +0900

    MathArrayUtilsMain 메서드 구현

--- src/main/java/static2/ex01/MathArrayUtilsMain.java
+++ src/main/java/static2/ex01/MathArrayUtilsMain.java
@@ -1,7 +1,16 @@
 package static2.ex01;
 
+import static static2.ex01.MathArrayUtils.*;
+
 public class MathArrayUtilsMain {
     public static void main(String[] args) {
-        
+        // 생성자를 private 으로 막아놨기 때문에 생성 자체가 되질 않음
+//        MathArrayUtils mathArrayUtils = new MathArrayUtils();
+
+        int[] values = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("sum= " + sum(values));
+        System.out.println("average= " + average(values));
+        System.out.println("min= " + min(values));
+        System.out.println("max= " + max(values));
     }
 }
